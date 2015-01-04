b6975229c0f56319243c28ffb818ef96a2eb5bd9
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2015-01-04 21:41:05 +0100

    Added MoveTemp() to use the "move" version of TArray::Append()

--- Source/GitSourceControl/Private/GitSourceControlUtils.cpp
+++ Source/GitSourceControl/Private/GitSourceControlUtils.cpp
@@ -522,14 +522,14 @@ bool RunDumpToFile(const FString& InPathToGitBinary, const FString& InRepository
 			FPlatformProcess::ReadPipeToArray(PipeRead, BinaryData);
 			if(BinaryData.Num() > 0)
 			{
-				BinaryFileContent.Append(BinaryData);
+                BinaryFileContent.Append(MoveTemp(BinaryData));
 			}
 		}
 		TArray<uint8> BinaryData;
 		FPlatformProcess::ReadPipeToArray(PipeRead, BinaryData);
 		if(BinaryData.Num() > 0)
 		{
-			BinaryFileContent.Append(BinaryData);
+            BinaryFileContent.Append(MoveTemp(BinaryData));
 		}
 		// Save buffer into temp file
 		if(FFileHelper::SaveArrayToFile(BinaryFileContent, *InDumpFileName))
