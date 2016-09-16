2900eef9d0de3df230cfcbf5d9823ee795d61a41
thk123 <thekileyenator@gmail.com>
2016-09-16 16:46:21 +0100

    Amended ChooseWD to reflect repos route file

--- Source/GitSourceControlDev/Private/InteractiveProcessChooseWD.cpp
+++ Source/GitSourceControlDev/Private/InteractiveProcessChooseWD.cpp
@@ -1,6 +1,6 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 
-#include "GitSourceControlPrivatePCH.h"
+#include "GitSourceControlDevPrivatePCH.h"
 #include "InteractiveProcessChooseWD.h"
 
 static FORCEINLINE bool CreatePipeWrite(void*& ReadPipe, void*& WritePipe)
