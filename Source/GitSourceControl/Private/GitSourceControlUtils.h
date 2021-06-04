8ab85d49310d37de6f26515fe15305e94673e27e
AzureAD\JimSimons <jim.simons@ultraleap.com>
2021-06-04 16:25:25 +0100

    Typos in comments

--- Source/GitSourceControl/Private/GitSourceControlUtils.h
+++ Source/GitSourceControl/Private/GitSourceControlUtils.h
@@ -36,7 +36,7 @@ struct FGitVersion;
 namespace GitSourceControlUtils
 {
 /**
-*  Returns an updated the repo root if all selected files are in a plugin subfolder, and the plugin subfolder is a git repo
+*  Returns an updated repo root if all selected files are in a plugin subfolder, and the plugin subfolder is a git repo
 *  This supports the case where each plugin is a sub module
 * 
 * @param AbsoluteFilePaths		The list of files in the SC operation
@@ -45,10 +45,10 @@ namespace GitSourceControlUtils
 FString ChangeRepositoryRootIfSubmodule(const TArray<FString>& AbsoluteFilePaths, const FString& PathToRepositoryRoot);
 
 /**
-*  Returns an updated the repo root if all selected file is in a plugin subfolder, and the plugin subfolder is a git repo
+*  Returns an updated repo root if all selected file is in a plugin subfolder, and the plugin subfolder is a git repo
 *  This supports the case where each plugin is a sub module
 *
-* @param AbsoluteFilePath		The files in the SC operation
+* @param AbsoluteFilePath		The file in the SC operation
 * @param PathToRepositoryRoot	The original path to the repository root (used by default)
 */
 FString ChangeRepositoryRootIfSubmodule(const FString& AbsoluteFilePath, const FString& PathToRepositoryRoot);
