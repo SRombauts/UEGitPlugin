Mu-L/UE4GitPlugin

Add support for UE5.3

Source/GitSourceControl/Private/GitSourceControlOperations.cpp
@@ -221,7 +221,11 @@ bool FGitCheckInWorker::Execute(FGitSourceControlCommand& InCommand)
 							"   git pull --rebase --autostash\n\n"
 							"Or run the equivalent in a Git GUI client of your choice"));
 						FText PushFailTitle(LOCTEXT("GitPush_OutOfDate_Title", "Git Pull Required"));
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MAJOR_VERSION >= 3
+						FMessageDialog::Open(EAppMsgType::Ok, PushFailMessage, PushFailTitle);
+#else
 						FMessageDialog::Open(EAppMsgType::Ok, PushFailMessage, &PushFailTitle);
+#endif
 						UE_LOG(LogSourceControl, Log, TEXT("Push failed because we're out of date, prompting user to resolve manually"));
 					}
 				}

Source/GitSourceControl/Private/GitSourceControlProvider.cpp
@@ -152,6 +152,23 @@ FText FGitSourceControlProvider::GetStatusText() const
 	return FText::Format(NSLOCTEXT("Status", "Provider: Git\nEnabledLabel", "Local repository: {RepositoryName}\nRemote origin: {RemoteUrl}\nUser: {UserName}\nE-mail: {UserEmail}\n[{BranchName} {CommitId}] {CommitSummary}"), Args);
 }
 
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MAJOR_VERSION >= 3
+
+TMap<ISourceControlProvider::EStatus, FString> FGitSourceControlProvider::GetStatus() const
+{
+	TMap<EStatus, FString> Result;
+	Result.Add(EStatus::Enabled, IsEnabled() ? TEXT("Yes") : TEXT("No") );
+	Result.Add(EStatus::Connected, (IsEnabled() && IsAvailable()) ? TEXT("Yes") : TEXT("No") );
+	Result.Add(EStatus::User, UserName);
+	Result.Add(EStatus::Repository, PathToRepositoryRoot);
+	Result.Add(EStatus::Remote, RemoteUrl);
+	Result.Add(EStatus::Branch, BranchName);
+	Result.Add(EStatus::Email, UserEmail);
+	return Result;
+}
+
+#endif
+
 /** Quick check if source control is enabled */
 bool FGitSourceControlProvider::IsEnabled() const
 {
@@ -287,6 +304,11 @@ ECommandResult::Type FGitSourceControlProvider::Execute(const TSharedRef<ISource
 		UE_LOG(LogSourceControl, Log, TEXT("IssueAsynchronousCommand(%s)"), *InOperation->GetName().ToString());
 		return IssueCommand(*Command);
 	}
+}
+
+bool FGitSourceControlProvider::CanExecuteOperation( const FSourceControlOperationRef& InOperation ) const
+{
+	return WorkersMap.Find(InOperation->GetName()) != nullptr;
 }
 
 bool FGitSourceControlProvider::CanCancelOperation(const FSourceControlOperationRef& InOperation) const

Source/GitSourceControl/Private/GitSourceControlProvider.h
@@ -62,6 +62,9 @@ public:
 	virtual void Init(bool bForceConnection = true) override;
 	virtual void Close() override;
 	virtual FText GetStatusText() const override;
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MAJOR_VERSION >= 3
+	virtual TMap<EStatus, FString> GetStatus() const override;
+#endif
 	virtual bool IsEnabled() const override;
 	virtual bool IsAvailable() const override;
 	virtual const FName& GetName(void) const override;
@@ -80,6 +83,7 @@ public:
 #else
 	virtual ECommandResult::Type Execute(const FSourceControlOperationRef& InOperation, const TArray<FString>& InFiles, EConcurrency::Type InConcurrency = EConcurrency::Synchronous, const FSourceControlOperationComplete& InOperationCompleteDelegate = FSourceControlOperationComplete() ) override;
 #endif
+	virtual bool CanExecuteOperation( const FSourceControlOperationRef& InOperation ) const; /* override	NOTE: added in UE5.3 */
 	virtual bool CanCancelOperation(const FSourceControlOperationRef& InOperation) const override;
 	virtual void CancelOperation(const FSourceControlOperationRef& InOperation) override;
 	virtual bool UsesLocalReadOnlyState() const override;

Source/GitSourceControl/Private/GitSourceControlState.cpp
@@ -47,6 +47,15 @@ TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> FGitSourceControlS
 	return nullptr;
 }
 
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MAJOR_VERSION >= 3
+
+ISourceControlState::FResolveInfo FGitSourceControlState::GetResolveInfo() const
+{
+	return PendingResolveInfo;
+}
+
+#else
+
 TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> FGitSourceControlState::GetBaseRevForMerge() const
 {
 	for (const auto& Revision : History)
@@ -60,6 +69,8 @@ TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> FGitSourceControlS
 
 	return nullptr;
 }
+
+#endif
 
 #if ENGINE_MAJOR_VERSION == 5
 

Source/GitSourceControl/Private/GitSourceControlState.h
@@ -60,7 +60,11 @@ public:
 	virtual TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> GetHistoryItem(int32 HistoryIndex) const override;
 	virtual TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> FindHistoryRevision(int32 RevisionNumber) const override;
 	virtual TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> FindHistoryRevision(const FString& InRevision) const override;
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MAJOR_VERSION >= 3
+	virtual FResolveInfo GetResolveInfo() const override;
+#else
 	virtual TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> GetBaseRevForMerge() const override;
+#endif
 	virtual TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> GetCurrentRevision() const; /* override	NOTE: added in UE5.2 */
 #if ENGINE_MAJOR_VERSION == 5
 	virtual FSlateIcon GetIcon() const override;
@@ -102,8 +106,13 @@ public:
 	/** Filename on disk */
 	FString LocalFilename;
 
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MAJOR_VERSION >= 3
+	/** Pending rev info with which a file must be resolved, invalid if no resolve pending */
+	FResolveInfo PendingResolveInfo;
+#else
 	/** File Id with which our local revision diverged from the remote revision */
 	FString PendingMergeBaseFileHash;
+#endif
 
 	/** State of the working copy */
 	EWorkingCopyState::Type WorkingCopyState;

Source/GitSourceControl/Private/GitSourceControlUtils.cpp
@@ -802,11 +802,30 @@ public:
 	/** Parse the unmerge status: extract the base SHA1 identifier of the file */
 	FGitConflictStatusParser(const TArray<FString>& InResults)
 	{
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MAJOR_VERSION >= 3
+		const FString& CommonAncestor = InResults[0]; // 1: The common ancestor of merged branches
+		CommonAncestorFileId = CommonAncestor.Mid(7, 40);
+		CommonAncestorFilename = CommonAncestor.Right(50);
+
+		if (ensure(InResults.IsValidIndex(2)))
+		{
+			const FString& RemoteBranch = InResults[2]; // 1: The common ancestor of merged branches
+			RemoteFileId = RemoteBranch.Mid(7, 40);
+			RemoteFilename = RemoteBranch.Right(50);
+		}
+#else
 		const FString& FirstResult = InResults[0]; // 1: The common ancestor of merged branches
 		CommonAncestorFileId = FirstResult.Mid(7, 40);
+#endif
 	}
 
 	FString CommonAncestorFileId;	///< SHA1 Id of the file (warning: not the commit Id)
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MAJOR_VERSION >= 3
+	FString RemoteFileId;		///< SHA1 Id of the file (warning: not the commit Id)
+
+	FString CommonAncestorFilename;
+	FString RemoteFilename;
+#endif
 };
 
 /** Execute a command to get the details of a conflict */
@@ -823,7 +842,14 @@ static void RunGetConflictStatus(const FString& InPathToGitBinary, const FString
 	{
 		// Parse the unmerge status: extract the base revision (or the other branch?)
 		FGitConflictStatusParser ConflictStatus(Results);
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MAJOR_VERSION >= 3
+		InOutFileState.PendingResolveInfo.BaseFile = ConflictStatus.CommonAncestorFilename;
+		InOutFileState.PendingResolveInfo.BaseRevision = ConflictStatus.CommonAncestorFileId;
+		InOutFileState.PendingResolveInfo.RemoteFile = ConflictStatus.RemoteFilename;
+		InOutFileState.PendingResolveInfo.RemoteRevision = ConflictStatus.RemoteFileId;
+#else
 		InOutFileState.PendingMergeBaseFileHash = ConflictStatus.CommonAncestorFileId;
+#endif
 	}
 }
 

SRombauts/UEGitPlugin

Source/GitSourceControl/Private/GitSourceControlOperations.cpp
@@ -221,7 +221,11 @@ bool FGitCheckInWorker::Execute(FGitSourceControlCommand& InCommand)
 							"   git pull --rebase --autostash\n\n"
 							"Or run the equivalent in a Git GUI client of your choice"));
 						FText PushFailTitle(LOCTEXT("GitPush_OutOfDate_Title", "Git Pull Required"));
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 3
+						FMessageDialog::Open(EAppMsgType::Ok, PushFailMessage, PushFailTitle);
+#else
 						FMessageDialog::Open(EAppMsgType::Ok, PushFailMessage, &PushFailTitle);
+#endif
 						UE_LOG(LogSourceControl, Log, TEXT("Push failed because we're out of date, prompting user to resolve manually"));
 					}
 				}

Source/GitSourceControl/Private/GitSourceControlProvider.cpp
@@ -152,6 +152,23 @@ FText FGitSourceControlProvider::GetStatusText() const
 	return FText::Format(NSLOCTEXT("Status", "Provider: Git\nEnabledLabel", "Local repository: {RepositoryName}\nRemote origin: {RemoteUrl}\nUser: {UserName}\nE-mail: {UserEmail}\n[{BranchName} {CommitId}] {CommitSummary}"), Args);
 }
 
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 3
+
+TMap<ISourceControlProvider::EStatus, FString> FGitSourceControlProvider::GetStatus() const
+{
+	TMap<EStatus, FString> Result;
+	Result.Add(EStatus::Enabled, IsEnabled() ? TEXT("Yes") : TEXT("No") );
+	Result.Add(EStatus::Connected, (IsEnabled() && IsAvailable()) ? TEXT("Yes") : TEXT("No") );
+	Result.Add(EStatus::User, UserName);
+	Result.Add(EStatus::Repository, PathToRepositoryRoot);
+	Result.Add(EStatus::Remote, RemoteUrl);
+	Result.Add(EStatus::Branch, BranchName);
+	Result.Add(EStatus::Email, UserEmail);
+	return Result;
+}
+
+#endif
+
 /** Quick check if source control is enabled */
 bool FGitSourceControlProvider::IsEnabled() const
 {
@@ -287,6 +304,11 @@ ECommandResult::Type FGitSourceControlProvider::Execute(const TSharedRef<ISource
 		UE_LOG(LogSourceControl, Log, TEXT("IssueAsynchronousCommand(%s)"), *InOperation->GetName().ToString());
 		return IssueCommand(*Command);
 	}
+}
+
+bool FGitSourceControlProvider::CanExecuteOperation( const FSourceControlOperationRef& InOperation ) const
+{
+	return WorkersMap.Find(InOperation->GetName()) != nullptr;
 }
 
 bool FGitSourceControlProvider::CanCancelOperation(const FSourceControlOperationRef& InOperation) const

Source/GitSourceControl/Private/GitSourceControlProvider.h
@@ -62,6 +62,9 @@ public:
 	virtual void Init(bool bForceConnection = true) override;
 	virtual void Close() override;
 	virtual FText GetStatusText() const override;
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 3
+	virtual TMap<EStatus, FString> GetStatus() const override;
+#endif
 	virtual bool IsEnabled() const override;
 	virtual bool IsAvailable() const override;
 	virtual const FName& GetName(void) const override;
@@ -80,6 +83,7 @@ public:
 #else
 	virtual ECommandResult::Type Execute(const FSourceControlOperationRef& InOperation, const TArray<FString>& InFiles, EConcurrency::Type InConcurrency = EConcurrency::Synchronous, const FSourceControlOperationComplete& InOperationCompleteDelegate = FSourceControlOperationComplete() ) override;
 #endif
+	virtual bool CanExecuteOperation( const FSourceControlOperationRef& InOperation ) const; /* override	NOTE: added in UE5.3 */
 	virtual bool CanCancelOperation(const FSourceControlOperationRef& InOperation) const override;
 	virtual void CancelOperation(const FSourceControlOperationRef& InOperation) override;
 	virtual bool UsesLocalReadOnlyState() const override;

Source/GitSourceControl/Private/GitSourceControlState.cpp
@@ -47,6 +47,15 @@ TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> FGitSourceControlS
 	return nullptr;
 }
 
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 3
+
+ISourceControlState::FResolveInfo FGitSourceControlState::GetResolveInfo() const
+{
+	return PendingResolveInfo;
+}
+
+#else
+
 TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> FGitSourceControlState::GetBaseRevForMerge() const
 {
 	for (const auto& Revision : History)
@@ -60,6 +69,8 @@ TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> FGitSourceControlS
 
 	return nullptr;
 }
+
+#endif
 
 #if ENGINE_MAJOR_VERSION == 5
 

Source/GitSourceControl/Private/GitSourceControlState.h
@@ -60,7 +60,11 @@ public:
 	virtual TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> GetHistoryItem(int32 HistoryIndex) const override;
 	virtual TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> FindHistoryRevision(int32 RevisionNumber) const override;
 	virtual TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> FindHistoryRevision(const FString& InRevision) const override;
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 3
+	virtual FResolveInfo GetResolveInfo() const override;
+#else
 	virtual TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> GetBaseRevForMerge() const override;
+#endif
 	virtual TSharedPtr<class ISourceControlRevision, ESPMode::ThreadSafe> GetCurrentRevision() const; /* override	NOTE: added in UE5.2 */
 #if ENGINE_MAJOR_VERSION == 5
 	virtual FSlateIcon GetIcon() const override;
@@ -102,8 +106,13 @@ public:
 	/** Filename on disk */
 	FString LocalFilename;
 
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 3
+	/** Pending rev info with which a file must be resolved, invalid if no resolve pending */
+	FResolveInfo PendingResolveInfo;
+#else
 	/** File Id with which our local revision diverged from the remote revision */
 	FString PendingMergeBaseFileHash;
+#endif
 
 	/** State of the working copy */
 	EWorkingCopyState::Type WorkingCopyState;

Source/GitSourceControl/Private/GitSourceControlUtils.cpp
@@ -802,11 +802,30 @@ public:
 	/** Parse the unmerge status: extract the base SHA1 identifier of the file */
 	FGitConflictStatusParser(const TArray<FString>& InResults)
 	{
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 3
+		const FString& CommonAncestor = InResults[0]; // 1: The common ancestor of merged branches
+		CommonAncestorFileId = CommonAncestor.Mid(7, 40);
+		CommonAncestorFilename = CommonAncestor.Right(50);
+
+		if (ensure(InResults.IsValidIndex(2)))
+		{
+			const FString& RemoteBranch = InResults[2]; // 1: The common ancestor of merged branches
+			RemoteFileId = RemoteBranch.Mid(7, 40);
+			RemoteFilename = RemoteBranch.Right(50);
+		}
+#else
 		const FString& FirstResult = InResults[0]; // 1: The common ancestor of merged branches
 		CommonAncestorFileId = FirstResult.Mid(7, 40);
+#endif
 	}
 
 	FString CommonAncestorFileId;	///< SHA1 Id of the file (warning: not the commit Id)
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 3
+	FString RemoteFileId;		///< SHA1 Id of the file (warning: not the commit Id)
+
+	FString CommonAncestorFilename;
+	FString RemoteFilename;
+#endif
 };
 
 /** Execute a command to get the details of a conflict */
@@ -823,7 +842,14 @@ static void RunGetConflictStatus(const FString& InPathToGitBinary, const FString
 	{
 		// Parse the unmerge status: extract the base revision (or the other branch?)
 		FGitConflictStatusParser ConflictStatus(Results);
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 3
+		InOutFileState.PendingResolveInfo.BaseFile = ConflictStatus.CommonAncestorFilename;
+		InOutFileState.PendingResolveInfo.BaseRevision = ConflictStatus.CommonAncestorFileId;
+		InOutFileState.PendingResolveInfo.RemoteFile = ConflictStatus.RemoteFilename;
+		InOutFileState.PendingResolveInfo.RemoteRevision = ConflictStatus.RemoteFileId;
+#else
 		InOutFileState.PendingMergeBaseFileHash = ConflictStatus.CommonAncestorFileId;
+#endif
 	}
 }
 